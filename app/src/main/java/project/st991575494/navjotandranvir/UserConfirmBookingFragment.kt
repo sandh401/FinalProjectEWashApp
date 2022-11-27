e69d2f88df21b5f8a224ee6351f9769d9cff3921
sandh401 <sandhunavjot504@gmail.com>
2022-11-27 15:04:25 -0800

    Confirm booking UI

---
+++ app/src/main/java/project/st991575494/navjotandranvir/UserConfirmBookingFragment.kt
@@ -0,0 +1,31 @@
+package project.st991575494.navjotandranvir
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class UserConfirmBookingFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = UserConfirmBookingFragment()
+    }
+
+    private lateinit var viewModel: UserConfirmBookingViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_user_confirm_booking, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(UserConfirmBookingViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/project/st991575494/navjotandranvir/UserConfirmBookingViewModel.kt
@@ -0,0 +1,7 @@
+package project.st991575494.navjotandranvir
+
+import androidx.lifecycle.ViewModel
+
+class UserConfirmBookingViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
